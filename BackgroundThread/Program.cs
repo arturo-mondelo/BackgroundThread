300c652fd6464b76c5f5d03760bd921ae6dc7638
amondelo <amondelo@hotmail.es>
2016-10-27 23:02:37 +0200

    Final

--- BackgroundThread/Program.cs
+++ BackgroundThread/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace BackgroundThread
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void ThreadMethod()
         {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+        
         }
     }
+    public static void Main()
+    {
+        Thread t = new Thread(new ThreadStart(ThreadMethod));
+        //t.IsBackground = true app sale al momento. = false crea un foreground thread e imprime el mensaje
+        t.IsBackground = false;
+        t.Start();
+    }
 }
+}
+
