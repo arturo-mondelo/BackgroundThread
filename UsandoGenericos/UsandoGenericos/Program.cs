e029ad798da89217f52c1564992eaf3fef326562
amondelo <amondelo@hotmail.es>
2016-11-18 23:08:28 +0100

    Ordenando usando genéricos

---
+++ UsandoGenericos/UsandoGenericos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsandoGenericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arrInts = new int[] { 2, 5, 4, 7, 1, 3, 9, 8 };
+            char[] arrChar = new char[] { 'f', 'a', 'r', 'c', 'h' };
+
+            //Ordenando
+        }
+    }
+}
